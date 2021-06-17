2fdf8de4a3e87958fa5924d6009f17dd6b6b37bd
tertek <ekin.tertemiz@swisstph.ch>
2021-06-17 11:42:25 +0200

    Add token endpoint logic.

--- endpoints/token.create.php
+++ endpoints/token.create.php
@@ -1,7 +1,27 @@
 <?php
 
-$user = "Test User";
+if( !$this->isValid($this->post['id']) ) {
+    RestUtility::sendResponse(400, "Bad Request - id is required");
+}
 
-$jwt = $this->generateToken($user);
+if( !$this->isValid($this->post['id']) ) {
+    RestUtility::sendResponse(400, "Bad Request - id is required");
+}
 
-$res = array("token" => $jwt);
+$id = $this->post['id'];
+$pass = $this->post['pass'];
+
+//  Fetch record_id with $id and $pass
+
+
+//$jwt = $this->generateToken($record_id);
+$jwt = '1234';
+
+$res = array(
+    "message" => "Success",
+    "id" => $id,
+    "pass" => $pass,
+    "token" => $jwt
+);
+
+//$res = array("token" => $jwt);
